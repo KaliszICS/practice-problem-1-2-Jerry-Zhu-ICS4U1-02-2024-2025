a2b6ddb8ea298022dfa6c20d63c7b47d52ed2d04
Jerry-Zhu-ICS4U1-02-2024-2025 <jerry.zhu2@student.tdsb.on.ca>
2025-03-06 14:38:10 +0000

    finish practice problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,32 @@ public class PracticeProblem {
 	}
 
 	
+public static boolean validIndex(int[] arr, int num){
+	try{
+		int num2 = arr[num];
+		return true;
+		
+	}
+	catch(ArrayIndexOutOfBoundsException e){
+		return false;
+	}
+}
 
+public static int divide(int num, int num2){
+	try{
+		return num/num2;
+	}
+	catch(ArithmeticException e){
+		return 0;
+	}
+}
+
+public static int safeConvertStringtoInt(String word){
+	try{
+		return Integer.parseInt(word);
+	}
+	catch(NumberFormatException e){
+return 0;
+	}
+}
 }
